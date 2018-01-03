1d9903331d63d12436d12dc726e4a8d495703310
Seth Gore <sethjgore@gmail.com>
2018-01-03 13:53:01 -0500

    updated

--- page-events-all.php
+++ page-events-all.php
@@ -6,6 +6,19 @@
 
 <?php get_header(); ?>
 
+<?php
+
+  $teachers = new WP_Query( [ 'post_type' => 'cycles', 'posts_per_page' => 1 ] );
+?>
+
+ <?php while( $teachers->have_posts() ) : $teachers->the_post(); ?>
+      
+
+      <?php the_title(); ?>
+
+<?php endwhile; ?>
+
+
 <div class="relative fb-1 flex-auto flex" style="background: url(), #e2e2e2 center center; background-size: cover;">
         <div class="flex-auto fb-1 mx2">
             <div class="height-full flex flex-column justify-end"> 
