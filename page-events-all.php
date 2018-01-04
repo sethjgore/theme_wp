ec39bb7962c6bf9a21c2acdf34fa09479478a03a
Seth Gore <sethjgore@gmail.com>
2018-01-04 12:32:24 -0500

    updated

--- page-events-all.php
+++ page-events-all.php
@@ -40,7 +40,7 @@
         </div>
         <div style="overflow-y: scroll !important; -webkit-overflow-scrolling: touch !important;" class="animated fadeIn fb-2 overflow-auto items-end flex-auto flex-column fb-1">
             <div class="p2 type-name-serif type-size subtitle flex black-trans">
-                <a href="<?php echo the_field('dhis_logo_link', 'option')" class="flex-auto fb-02 items-center flex justify-end text-decoration-none"><img src="<?php echo THEME_IMAGES;?>/logo-dhis.png" class="type-size-subtitle height-three mx2">
+                <a href="<?php echo the_field('dhis_logo_link', 'option') ?>" class="flex-auto fb-02 items-center flex justify-end text-decoration-none"><img src="<?php echo THEME_IMAGES;?>/logo-dhis.png" class="type-size-subtitle height-three mx2">
                 <div class="flex items-center">
             <img src="<?php echo THEME_IMAGES;?>/right-arrow.svg" class="type-size-paragraph width-two">
         </div></a></div>
