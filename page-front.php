ff0c352c457f470ad95ac5f96ae2e71c5cfe1d53
Seth Gore <sethjgore@gmail.com>
2018-01-04 17:35:13 -0500

    updated

--- page-front.php
+++ page-front.php
@@ -2,6 +2,12 @@
  /* Template Name: Front Page */
 
  define( 'THEME_IMAGES', get_template_directory_uri().'/images' );
+
+ $options = get_field('page_front', 'option');
+ $images = get_field('page_front_site');
+ echo print_r($images);
+ // $image = array_rand()
+
 ?>
 
 <?php get_header(); ?>
@@ -13,7 +19,7 @@
     <div class="slight-opacity flex height-full items-start p2"><img src="<?php echo THEME_IMAGES;?>/logo-dhis.png" class="type-size-two height-three mx2"></div>
 </div>
         <div class="flex justify-center items-center flex-auto fb-2 ">
-        <div style="background: url(http://allthebuildingsinnewyork.com/new/wp-content/uploads/2016/08/jgh_town_19greenest_low.jpg) center center no-repeat; background-size: contain;" class="flex-auto fb-1 animated fadeIn height-full"></div>
+        <div style="background: url(<?php echo $image->URL;?>) center center no-repeat; background-size: contain;" class="flex-auto fb-1 animated fadeIn height-full"></div>
       </div>
         <div class="items-center animated fadeInUp  flex quote justify-around flex-auto my2 fb-04 flex">
             <img src="<?php echo THEME_IMAGES;?>/logo-green-large-black.svg" class="type-size-one height-three ml2">
