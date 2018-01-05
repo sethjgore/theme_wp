99aa79be7c9250ea03d1f4d5c235376346e3958d
Seth Gore <sethjgore@gmail.com>
2018-01-05 09:46:27 -0500

    updated

--- header.php
+++ header.php
@@ -8,7 +8,13 @@
 	<!--[if lt IE 9]>
 		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
+	<link rel="manifest" href="<?php echo get_template_directory_uri();?>/manifest.json">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
 	</head>
+
 	<body <?php body_class(); ?>>
     <header class="container-fluid">
     </header>
