5557ea64f28c9b256b084bcd2ab3fe952bfe1aea
Seth Gore <sethjgore@gmail.com>
2018-01-05 12:57:05 -0500

    updated

--- contact.php
+++ contact.php
@@ -22,10 +22,6 @@ define( 'WP_DEBUG', true );
 </script>" ?>
 
 <div class="flex-column bg-g4 height-full flex items-center justify-center absolute z4 width-full-viewport height-full-viewport top-0 right-0 left-0">
-        <div class="absolute z0 width-full height-full bottom-0 top-0 right-0 left-0"></div>
-        <div class="flex-auto fb-02 flex width-full justify-end items-start">
-        <div class="m2 button type-name-sans type-size-paragraph px3 bg-black button-pill white flex items-center justify-center"><img class="type-size-subtitle height-three" src="<?php echo THEME_IMAGES; ?>/cancel.svg"></div>
-        </div>
         <div class="flex-auto fb-1 flex bg-gray-1">
         <div class="flex flex-column items-start justify-center pr2 pl3 flex-auto fb-05 bg-green-1">
             <div class="bold line-height-2 type-size-title type-name-sans white">
