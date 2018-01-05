14e3673853065c1ff4f87d7502c0c43f18ea4f1a
Seth Gore <sethjgore@gmail.com>
2018-01-04 19:33:32 -0500

    updated

--- modal.php
+++ modal.php
@@ -9,7 +9,7 @@
 <div class="flex-column bg-g4 height-full flex items-center justify-center absolute z4 width-full-viewport height-full-viewport top-0 right-0 left-0">
         <div class="absolute z0 width-full height-full bottom-0 top-0 right-0 left-0"></div>
         <div class="flex-auto fb-02 flex width-full justify-end items-start">
-        <div class="m2 button type-name-sans type-size-paragraph px3 bg-black button-pill white flex items-center justify-center"><img class="type-size-subtitle height-three" src="/cancel.svg"></div>
+        <div class="m2 button type-name-sans type-size-paragraph px3 bg-black button-pill white flex items-center justify-center"><img class="type-size-subtitle height-three" src="<?php echo THEME_IMAGES; ?>/cancel.svg"></div>
         </div>
         <div class="flex-auto fb-1 flex items-center justify-center">
         <div class="bg-white animated fadeInLeft overflow-hidden relative z1">
