e5963de4094aa0e6187e53b55ac6f56ee642905a
Seth Gore <sethjgore@gmail.com>
2018-01-09 08:45:59 -0500

    updated

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -8,7 +8,7 @@ let toggle = function(item){
 	if(document.querySelector(item)){
 		document.querySelector(item).classList.toggle('hide');
 		let animation = document.querySelector('[visible]').getAttribute('visible');
-		document.querySelector('[visible]').classList.toggle('animation');
+		document.querySelector('[visible]').classList.toggle(animations);
 		debugger;
 	}else{
 		console.log(item+ 'is not a selector in document');
