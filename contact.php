f25a4916a1d1f1d00f62776b0e84b472f082d4d8
Seth Gore <sethjgore@gmail.com>
2018-01-10 08:21:26 -0500

    updated

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@ define( 'WP_DEBUG', true );
                 Send your event and get people to come!
             </div>
         </div>
-        <div id="deafnyc-contact" class="bg-white animated slideInRight bg-white flex-auto fb-1 overflow-auto " style="-webkit-overflow-scrolling: touch !important;">
+        <div id="deafnyc-contact" class="bg-white animated fadeIn bg-white flex-auto fb-1 overflow-auto " style="-webkit-overflow-scrolling: touch !important;">
         <div id="popup-id" class="relative z1">
             <div class="py1 bg-blue-royal
             center bold type-name-sans type-size-title white">New Event Form
