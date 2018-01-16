5b4e00849296b7478ca55642ec8f99a9a5908868
Seth Gore <sethjgore@gmail.com>
2018-01-16 14:12:29 -0500

    updated

--- single-events.php
+++ single-events.php
@@ -196,11 +196,11 @@ if( $options ): ?>
 
 
         <div onclick="toggle('#modalContact')" class="absolute z0 width-full height-full bottom-0 top-0 right-0 left-0"></div>
-        <div class="flex-auto fb-02 flex width-full justify-end items-start">
+        <div class="flex-auto fb-01 flex width-full justify-end items-start">
         <div onclick="toggle('#modalContact')" class="m2 button type-name-sans type-size-paragraph px3 bg-black button-pill white flex items-center justify-center"><img class="type-size-subtitle height-three" src="<?php echo THEME_IMAGES;?>/cancel.svg"></div>
         </div>
         <div class="flex-auto fb-1 flex items-center justify-center">
-        <div visible="fadeInLeft" class="bg-white animated overflow-hidden relative z1 flex flex-column">
+        <div visible="slideIn" class="bg-white animated overflow-hidden relative z1 flex flex-column">
          <img class="height-full" src="<?php  $i = get_field('graphic')['url']; echo $i;?>">
 
     </div>
@@ -214,7 +214,7 @@ if( $options ): ?>
 <div id="modalContact" class="hide flex-column bg-g4 height-full flex items-center justify-center absolute z4 width-full-viewport height-full-viewport top-0 right-0 left-0">
 
         <div onclick="toggle('#modalContact')" class="absolute z0 width-full height-full bottom-0 top-0 right-0 left-0"></div>
-        <div class="flex-auto fb-02 flex width-full justify-end items-start">
+        <div class="flex-auto fb-01 flex width-full justify-end items-start">
         <div onclick="toggle('#modalContact')" class="m2 button type-name-sans type-size-paragraph px3 bg-black button-pill white flex items-center justify-center"><img class="type-size-subtitle height-three" src="<?php echo THEME_IMAGES;?>/cancel.svg"></div>
         </div>
         <div class="flex-auto fb-1 flex items-center justify-center">
