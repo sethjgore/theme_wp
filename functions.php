4d475c71f2adbc7bfa20321889cada4bf55ff38a
Seth Gore <sethjgore@gmail.com>
2018-01-16 17:27:42 -0500

    updated

--- functions.php
+++ functions.php
@@ -75,16 +75,5 @@ wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/modal.js
 dsb_theme_init();
 
 
-function create_weeklyfeed() {
-load_template( TEMPLATEPATH . 'rss-weekly.php'); // You'll create a your-custom-feed.php file in your theme's directory
-
-}
-add_action('do_feed_weeklyfeed', 'create_weeklyfeed', 10, 1); // Make sure to have 'do_feed_customfeed'
-
-add_action('do_feed_super', 'my_custom_rss_render', 10, 1); // Make sure to have 'do_feed_customfeed'
-
-function my_custom_rss_render() {
-    get_template_part( 'feed', 'weekly' );
-}
 
 ?>
