c4108b9fd1381736e7789b0234e60ada0217d87e
Seth Gore <sethjgore@gmail.com>
2018-01-17 12:03:08 -0500

    updated

--- feed-weekly.php
+++ feed-weekly.php
@@ -59,7 +59,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
         <item>
           <link><?php echo get_post_permalink($event->ID) ?></link>
            <title><?php echo $event->title; ?> </title>
-           <pubDate><?php echo the_field('date', $event->ID) ?> @ <?php timed($start,$end);?></pubDate>
+           <source><?php echo the_field('date', $event->ID) ?> @ <?php timed($start,$end);?></source>
         <description><![CDATA[<?php echo $event->description; ?>]]></description>
                 <?php
                 /**
