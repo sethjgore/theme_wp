ce692c7c4bc56c6177b867e287e68bf8a0f3d5d6
Seth Gore <sethjgore@gmail.com>
2018-01-17 12:04:02 -0500

    updated

--- feed-weekly.php
+++ feed-weekly.php
@@ -59,7 +59,10 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
         <item>
           <link><?php echo get_post_permalink($event->ID) ?></link>
            <title><?php echo $event->title; ?> </title>
-           <source><?php echo the_field('date', $event->ID) ?> @ <?php timed($start,$end);?></source>
+           <source>
+                <title><?php echo the_field('date', $event->ID) ?> @ <?php timed($start,$end);?></title>
+                <url>www.deafnyc.com</url>
+        </source>
         <description><![CDATA[<?php echo $event->description; ?>]]></description>
                 <?php
                 /**
